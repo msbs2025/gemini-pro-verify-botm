2d63c707dd23b7165830be2aed7070235ed5775a
msbs2025 <mahdi.sp.ir@gmail.com>
2026-09-24 10:34:31 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def main():
     commands.db = db
     
     # Create application
-    application = Application.builder().token(BOT_TOKEN).build()
+    application = Application.builder().token(8747151834:AAEFaQtawvZlJzpR5SDosXvD1vIWMFVlygI).build()
     
     # Register command handlers
     application.add_handler(CommandHandler("start", start_command))
