2241494a3e6fd1121fb0f3cb8fe26109cb07cad8
msbs2025 <mahdi.sp.ir@gmail.com>
2026-09-24 08:59:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ except ImportError:
     # During scaffold verification, python-dotenv might not be installed in the environment
     pass
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8984637564:AAGs5P5Nb7zUyp551TOXbqsMz95Tw62j10A")
